cd1e1d24264662d3f6605c2cad814913e3925729
Dave Ring <davealring@gmail.com>
2024-07-11 10:33:07 -0400

    Finished 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+// const removeFromArray = function(inputArray, ...inputArg) {
+//     let loopLength = inputArray.length;
+//     for (i=0; i<loopLength;i++) {
+//         if (inputArray[i] == inputArg) {
+//             inputArray.splice(i,1);
+//         };
+//     };
+//     return inputArray;
+// };
+
+const removeFromArray = function(inputArray, ...inputArg) {
+    let newArray = [];
+    inputArray.forEach((item)=> {
+        if(!inputArg.includes(item)){
+            newArray.push(item);
+        };
+    });
+    return newArray;
 };
 
 // Do not edit below this line
