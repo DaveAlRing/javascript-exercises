ce491cc470655c0879db9926660fafcbd07dc8cb
Dave Ring <davealring@gmail.com>
2024-07-11 09:23:50 -0400

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    let container = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    while(i<num){
+        i++;
+        container += string;
+    };
+    return container;
 };
 
 // Do not edit below this line
