26492812322e44a569f049185869c29397ec538a
Dave Ring <davealring@gmail.com>
2024-07-11 11:14:59 -0400

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(integerOne, integerTwo) {
+    let sum = 0;
+    let i = 0;
+    if((integerOne || integerTwo) < 0) {
+        return "ERROR";
+    }
+    if(!Number.isInteger(integerOne) || !Number.isInteger(integerTwo)) {
+        return "ERROR";
+    }
+    if(integerOne<integerTwo) {
+        while (i<integerTwo) {
+            sum += (integerOne + i);
+            i++;
+        }
+        return sum;
+    } else {
+        while (i<integerOne) {
+            sum += (integerTwo + i);
+            i++;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
