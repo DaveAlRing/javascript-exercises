35cdc9e60c4c329638565b725a44ebd55c344edb
Dave Ring <davealring@gmail.com>
2024-07-11 09:54:09 -0400

    Finished 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let inputArray = [];
+    let loopLength = inputString.length;
+    let outputString = '';
+    for (let i=0; i<loopLength;i++) {
+        inputArray += inputString.charAt(i);
+    };
+    for (let j=loopLength -1; j>=0;j--){
+        outputString += inputArray[j];
+    };
+    return outputString;
 };
 
 // Do not edit below this line
