40b8e28abf978fabd2b6242a3d2c00e18e2315cc
Dave Ring <davealring@gmail.com>
2024-07-30 11:11:30 -0400

    10 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let alpha = 'abcdefghijklmnopqrstuvwxyz123456789';
+    let cleanedArray = inputString
+                        .toLowerCase()
+                        .split('')
+                        .filter(char => alpha.includes(char))
+                        .join('');
+    let reversedArray = cleanedArray.split('').reverse().join('');
+    return charArray === reversedArray;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let total = 0;
+    let preTotal = 1;
+    if(num<0){
+        return "OOPS";
+    }
+    for(let i=1; i<=num; i++) {
+        let varC = total;
+        total += preTotal;
+        preTotal = varC;
+    }
+    return total;
 };
 
 // Do not edit below this line
